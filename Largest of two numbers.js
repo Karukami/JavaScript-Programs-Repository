8db8df7e9e505b52ff82a6fbc34eb9ed7432bd49
Karukami <lobsang_daniel@outlook.es>
2019-09-03 09:06:09 -0400

    Added "Largest of two numbers.js"

---
+++ Largest of two numbers.js
@@ -0,0 +1,12 @@
+var num1 = 9;
+var num2 = 2;
+
+if (num1 > num2) {
+	console.log("The largest number is: " + num1);
+} 
+else if (num1 < num2) {
+	console.log("The largest number is: " + num2);
+}
+else {
+	console.log("Both are equal.");
+}
